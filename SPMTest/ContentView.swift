56c585343169b4965fe137d0c558ab501145e8ce
Tony <chengkaizone@gmail.com>
2019-10-29 20:12:21 +0800

    Initial Commit

---
+++ SPMTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPMTest
+//
+//  Created by tony on 2019/10/29.
+//  Copyright © 2019 tony. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
